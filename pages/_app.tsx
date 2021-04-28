00253485572812d8be4559e92e4eac69adf89936
honghainguyen777 <n.h.hai2015@gmail.com>
2021-04-28 20:52:44 +0200

    Adding styles to tasks listing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,15 @@ import "../styles/globals.css";
 import { AppProps } from "next/app";
 import { useApollo } from "../lib/client";
 import { ApolloProvider } from "@apollo/client";
+import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const ApolloClient = useApollo(pageProps.initialApolloState);
   return (
     <ApolloProvider client={ApolloClient}>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ApolloProvider>
   );
 }
